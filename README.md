f3b391f0f3b14e81f97eb1b4fc39f116fa20a635
Adam Wenger <adamwenger44@gmail.com>
2019-12-16 11:11:04 -0800

    prompts working

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # developer_profile_generator.github.io
+
+will take in user input to populate a html page with that data
---
+++ index.js
@@ -0,0 +1,58 @@
+const inquirer = require("inquirer");
+// const fs = require("fs");
+// const util = require("util");
+
+// const writeFileAsync = util.promisify(fs.writeFile);
+
+function promptUser() {
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "What is your github username?"
+        },
+        {
+            type: "list",
+            message: "what is your favorite color?",
+            name: "color",
+            choices: [
+                "green",
+                "blue",
+                "purple",
+                "orange",
+                "pink"
+            ]
+        }
+    ])
+}
+
+function generateHTML(answers) {
+    return `
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <title>Document</title>
+  </head>
+  <body>
+    <div class="jumbotron jumbotron-fluid">
+    <div class="container">
+      <h1 class="display-4">Hi! My name is ${answers.name}</h1>
+      <p class="lead">I am from ${answers.location}.</p>
+      <h3>Example heading <span class="badge badge-secondary">Contact Me</span></h3>
+      <ul class="list-group">
+        <li class="list-group-item">My GitHub username is ${answers.github}</li>
+        <li class="list-group-item">LinkedIn: ${answers.linkedin}</li>
+      </ul>
+    </div>
+  </div>
+  </body>
+  </html>`;
+  }
+
+promptUser()
+.then(function(answers) {
+
+})
